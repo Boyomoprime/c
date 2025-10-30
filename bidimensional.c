d880fa9ecf32c396bf21c535ec7dabbad9af9892
Boyomoprime <boyomoprime@gmail.com>
2025-10-30 10:36:38 +0100

    Add files via upload

---
+++ bidimensional.c
@@ -0,0 +1,15 @@
+/***********************************
+ * Escriu del 10 al 1 per terminal *
+ * Marcos Lli - 1bat - PSIX - 25-26*
+ * *******************************/
+#include<stdio.h>
+void main(){
+    int i;
+    for(i=1;i<=10;i++)
+    {
+    for(i=1;1<=10;++)
+        printf("%d, ",i);
+    printf("\n");
+    }
+    return;
+}
